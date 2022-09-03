1aa5eab642b26dacfc0ee2779a6772c787ebbf11
Farhad <farhadmolla618@gmail.com>
2022-09-03 09:29:13 +0530

    user login setup

--- src/main/java/org/chaterbox/Main.java
+++ src/main/java/org/chaterbox/Main.java
@@ -5,11 +5,17 @@ import org.bson.Document;
 public class Main {
     public static void main(String[] args) {
 //        Connection connection = new Connection();
-//
 //        Document sampleDoc = new Document().append("name", "js").append("chat", "55");
-//
 //        connection.col.insertOne(sampleDoc);
-        UserSignUp usu = new UserSignUp("sdf", "sdf", "1234563");
-        usu.addUser();
+
+//        UserSignUp usu = new UserSignUp("sdf", "sdf", "1234563");
+//        usu.addUser();
+
+//        UserLogin uli = new UserLogin("sdf", "sdf");
+//        String s = uli.loginKaro();
+//        System.out.println(s);
+
+
+
     }
 }
--- src/main/java/org/chaterbox/UserLogin.java
+++ src/main/java/org/chaterbox/UserLogin.java
@@ -1,7 +1,43 @@
 package org.chaterbox;
 
+import com.mongodb.MongoException;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import javax.management.QueryExp;
+
+import static com.mongodb.client.model.Filters.eq;
+import static javax.management.Query.and;
+
 public class UserLogin {
-    Connection connection = new Connection();
+    private Connection connection = new Connection();
+    private String userName;
+    private String password;
 
+    UserLogin (String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 
+    String loginKaro() {
+        Bson projectionFields = Projections.fields(
+                Projections.include("user-name", "password"),
+                Projections.excludeId());
+
+        Document doc2 = (Document) connection.users.find(eq("user-name", userName))
+                .projection(projectionFields)
+                .first();
+
+        // System.out.println(doc2.get("password").equals(password));
+
+        if (doc2 == null || !(doc2.get("password").equals(password))) {
+            System.out.println("User name or password does not matched.");
+        } else {
+            return userName;
+        }
+        return null;
+    }
 }
